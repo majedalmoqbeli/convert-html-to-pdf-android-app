cde190309c36f71a1cac17db846cce238b990e77
majedalmoqbeli <majedalmoqbeli@gmail.com>
2023-11-08 14:15:27 +0300

    [new task] support dataBinding and viewBinding

--- app/src/main/java/com/majedalmoqbeli/html_to_pdf/MainActivity.java
+++ app/src/main/java/com/majedalmoqbeli/html_to_pdf/MainActivity.java
@@ -3,12 +3,19 @@ package com.majedalmoqbeli.html_to_pdf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.majedalmoqbeli.html_to_pdf.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 }
