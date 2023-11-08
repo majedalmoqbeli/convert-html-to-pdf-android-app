f285bab70c36e61d4eff8588121847618a48ea74
majedalmoqbeli <majedalmoqbeli@gmail.com>
2023-11-08 14:08:22 +0300

    first commit

---
+++ app/src/main/java/com/majedalmoqbeli/html_to_pdf/MainActivity.java
@@ -0,0 +1,14 @@
+package com.majedalmoqbeli.html_to_pdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
